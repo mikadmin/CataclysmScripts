075c1ee4d64a00fb6331063cb212da7a27d38fe6
Naios <naios-dev@live.de>
2011-11-28 20:46:28 +0100

    Batch Error Fixxed
    Signed-off-by: Naios <naios-dev@live.de>

--- Firelands/SQL/Firelands Instance Update.sql
+++ Firelands/SQL/Firelands Instance Update.sql
@@ -3,10 +3,10 @@
 ## Firelands Updates by Naios ##
 ##############################*/
 
-SET @SHANNOX_SCRIPT_TEXTS_ENTRY = -1999970
+SET @SHANNOX_SCRIPT_TEXTS_ENTRY = -1999970;
 
 -- ### Instance Template ###
-DELETE FROM `instance_template` WHERE `map`= 720;
+DELETE FROM `instance_template` WHERE map = 720;
 INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES (720, 0, 'instance_firelands', 1);
 
 -- ### Set Scriptnames ###
@@ -18,6 +18,6 @@ UPDATE `creature_template` SET `ScriptName`='npc_shannox_spear' WHERE `entry`=53
 -- ### Set Script_Texts ###
 -- # Shannox #
 -- Enrage Warning
-DELETE FROM `script_texts` WHERE `entry` = @SHANNOX_SCRIPT_TEXTS_ENTRY and `npc_entry` = 53691;
+DELETE FROM `script_texts` WHERE `entry` = @SHANNOX_SCRIPT_TEXTS_ENTRY and npc_entry = 53691;
 
 INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES (53691, @SHANNOX_SCRIPT_TEXTS_ENTRY, 'Shannox becomes enraged at seeing once of his companions fall!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 41, 0, 0, 'Shannox Enrage Warning');
