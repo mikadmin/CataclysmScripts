49fce821e6f5c85fd7e3dbffa744ffadc8b1b475
Naios <naios-dev@live.de>
2011-11-29 15:56:22 +0100

    Cleanup, Fixxes, Sounds, Added Action when Dogs are Dead Updated Access Requirement
    Signed-off-by: Naios <naios-dev@live.de>

--- Firelands/SQL/Firelands Instance Update.sql
+++ Firelands/SQL/Firelands Instance Update.sql
@@ -5,9 +5,15 @@
 
 SET @SHANNOX_SCRIPT_TEXTS_ENTRY = -1999970;
 
--- ### Instance Template ###
+-- ### Instance Template & Access Requirement ###
 DELETE FROM `instance_template` WHERE map = 720;
 INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES (720, 0, 'instance_firelands', 1);
+
+DELETE FROM `access_requirement` WHERE `mapId` = 720;
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `quest_failed_text`, `comment`) VALUES (720, 0, 85, 0, 0, 0, 0, 0, 0, NULL, 'Firelands (Enterence)');
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `quest_failed_text`, `comment`) VALUES (720, 1, 85, 0, 0, 0, 0, 0, 0, NULL, 'Firelands (Enterence)');
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `quest_failed_text`, `comment`) VALUES (720, 2, 85, 0, 0, 0, 0, 0, 0, NULL, 'Firelands (Enterence)');
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `quest_failed_text`, `comment`) VALUES (720, 3, 85, 0, 0, 0, 0, 0, 0, NULL, 'Firelands (Enterence)');
 
 -- ### Set Scriptnames ###
 UPDATE `creature_template` SET `ScriptName`='boss_shannox' WHERE `entry`=53691 LIMIT 1;
@@ -19,5 +25,13 @@ UPDATE `creature_template` SET `ScriptName`='npc_shannox_spear' WHERE `entry`=53
 -- # Shannox #
 -- Enrage Warning
 DELETE FROM `script_texts` WHERE `entry` = @SHANNOX_SCRIPT_TEXTS_ENTRY and npc_entry = 53691;
+INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES (53691, @SHANNOX_SCRIPT_TEXTS_ENTRY, 'A-hah! The interlopers! Kill them EAT THEM!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 24517, 0, 0, 0, 'Shannox Aggro Yell');
 
-INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES (53691, @SHANNOX_SCRIPT_TEXTS_ENTRY, 'Shannox becomes enraged at seeing once of his companions fall!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 41, 0, 0, 'Shannox Enrage Warning');
+DELETE FROM `script_texts` WHERE `entry` = @SHANNOX_SCRIPT_TEXTS_ENTRY-1 and npc_entry = 53691;
+INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES (53691, @SHANNOX_SCRIPT_TEXTS_ENTRY-1, 'Shannox becomes enraged at seeing once of his companions fall!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 41, 0, 0, 'Shannox Enrage Warning');
+
+DELETE FROM `script_texts` WHERE `entry` = @SHANNOX_SCRIPT_TEXTS_ENTRY-2 and npc_entry = 53691;
+INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES (53691, @SHANNOX_SCRIPT_TEXTS_ENTRY-2, 'Oh, you murders! Why? Why would you kill such a nobie animal?', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 24517, 0, 0, 0, 'Shannox Yell on killing a Dog');
+
+DELETE FROM `script_texts` WHERE `entry` = @SHANNOX_SCRIPT_TEXTS_ENTRY-3 and npc_entry = 53691;
+INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES (53691, @SHANNOX_SCRIPT_TEXTS_ENTRY-3, 'Ohh... the pain   Lord of Fire. It hurts...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 24517, 0, 0, 0, 'Shannox Dead Yell');
--- Firelands/boss_shannox.cpp
+++ Firelands/boss_shannox.cpp
@@ -19,16 +19,19 @@
 * with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-/* ########################################
+/*###########################
+#                           #
+#   Developer Info:         #
+#   Script Coded by Naios   #
+#   11/2011                 #
+#                           #
+#   Script Complete: 20%    #
+#   Tested on Trillium EMU  #
+#                           #
+###########################*/
 
-Developer Info:
-Script Coded by Naios
-11/2011
 
-Script Complete: 15%
-
-Tested on Trillium EMU
-
+/*
 Notes:
 
 Shannox 53691
@@ -74,44 +77,44 @@ Your face is being mauled.
 
 enum Yells //need texts and sound ids
 {
-	//SAY_AGGRO = -0, //sound id 24517
-	 SAY_AGGRO                                   = -1999900, //TODO this is only an Example Value
-	 SAY_ENRAGE									 = -1999970, //TODO Add Sound
-
+	SAY_AGGRO                                    = -1999970,
+	SAY_SOFT_ENRAGE								 = -1999971, //TODO Add Sound
+	SAY_ON_DOGS_FALL							 = -1999972, //TODO Add Sound
+	SAY_ON_DEAD									 = -1999973, //TODO Add Sound
 };
 
 enum Spells
 {
 
-//Shannox
- SPELL_ARCTIC_SLASH = 99931, // Inflicts 125% of weapon damage to enemies in front of the caster, and causes a Jagged Tear on the caster's primary target. (Trigger Spell Jagged Tear)
- SPELL_BERSERK = 26662, 
- SPELL_CALL_SPEAR = 100663, // Script Effect Value: 49575
- SPELL_HURL_SPEAR = 100002, // Dummy Effect & Damage
- SPELL_HURL_SPEAR_SUMMON = 99978, //Summons Spear of Shannox
- SPELL_HURL_SPEAR_DUMMY_SCRIPT = 100031, //Dummy Value: 99978
- SPELL_JAGGED_TEAR = 99937, //Deals 2500 damage every 3 sec for 24 sec. Stacks.
- SPELL_MAGMA_RUPTURE_SHANNOX = 99840, //Causes a massive explosion of magma, dealing 47500 to 52500 Fire damage to all nearby enemies, increasing Fire damage taken by 40% for 1 min, and triggering eruptions of magma. 
- SPELL_SEPERATION_ANXIETY = 99835, //When the distance between the huntsman and his pets grows too great, both become extremely agitated, increasing their damage and attack speed by 100%. (Effect #1	Apply Aura: Mod Damage Done % (127) Value: 100 Effect #2	Apply Aura: ?? (Aura #319) Value: 100)
- SPELL_FRENZY_SHANNOX = 23537,// Increases the caster's attack speed by 50% and the Physical damage it deals by 219 to 281 for 10 min. (Spell Aura = 9204)
+	//Shannox
+	SPELL_ARCTIC_SLASH = 99931, // Inflicts 125% of weapon damage to enemies in front of the caster, and causes a Jagged Tear on the caster's primary target. (Trigger Spell Jagged Tear)
+	SPELL_BERSERK = 26662, 
+	SPELL_CALL_SPEAR = 100663, // Script Effect Value: 49575
+	SPELL_HURL_SPEAR = 100002, // Dummy Effect & Damage
+	SPELL_HURL_SPEAR_SUMMON = 99978, //Summons Spear of Shannox
+	SPELL_HURL_SPEAR_DUMMY_SCRIPT = 100031, //Dummy Value: 99978
+	SPELL_JAGGED_TEAR = 99937, //Deals 2500 damage every 3 sec for 24 sec. Stacks.
+	SPELL_MAGMA_RUPTURE_SHANNOX = 99840, //Causes a massive explosion of magma, dealing 47500 to 52500 Fire damage to all nearby enemies, increasing Fire damage taken by 40% for 1 min, and triggering eruptions of magma. 
+	SPELL_SEPERATION_ANXIETY = 99835, //When the distance between the huntsman and his pets grows too great, both become extremely agitated, increasing their damage and attack speed by 100%. (Effect #1	Apply Aura: Mod Damage Done % (127) Value: 100 Effect #2	Apply Aura: ?? (Aura #319) Value: 100)
+	SPELL_FRENZY_SHANNOX = 23537,// Increases the caster's attack speed by 50% and the Physical damage it deals by 219 to 281 for 10 min. (Spell Aura = 9204)
 
- //Misc
- SPELL_IMMOLATION_TRAP = 52606, //Place a fire trap that will burn the first enemy to approach for <Unknown value - The spell referenced no longer exists> Fire damage over <Unknown value - The spell referenced no longer exists>.  Trap will exist for 30 sec.  Only one trap can be active at a time.(Drop Trap (181030))
- 
- //Riplimb
- SPELL_DOGGED_DETERMINATION = 101111, //Filled with a sense of purpose, the hound resists attempts to hinder its path back to its master.
- SPELL_FRENZY_RIPLIMB = 100522, //Increases attack speed by 30% and physical damage dealt by 30%.
- SPELL_LIMB_RIP = 99832, //The hellhound tears viciously at an enemy's exposed flesh, dealing 130% of normal damage and causing a bleeding wound that deals 0 damage every 5 sec for until cancelled. (Triggers Jagged Tear)
- SPELL_FRENZIED_DEVOLUTION = 100064, //Increases the caster's attack and movement speeds by 200% and all damage it deals by 400%. (Applys some Auras)
- SPELL_FEEDING_FRENZY_H = 100655, //Successful melee attacks drive the hound into a feeding frenzy, granting a stacking 5% bonus to physical damage dealt for 20 sec.
+	//Misc
+	SPELL_IMMOLATION_TRAP = 52606, //Place a fire trap that will burn the first enemy to approach for <Unknown value - The spell referenced no longer exists> Fire damage over <Unknown value - The spell referenced no longer exists>.  Trap will exist for 30 sec.  Only one trap can be active at a time.(Drop Trap (181030))
 
- //Rageface
- SPELL_FACE_RAGE = 99947, //Pins an enemy to the ground, stunning them and tearing at their flesh to deal 38 to 42 damage every 0.5 sec for 30 sec.
- BUFF_FACE_RAGE = 100129, // I'll hope this is correct
+	//Riplimb
+	SPELL_DOGGED_DETERMINATION = 101111, //Filled with a sense of purpose, the hound resists attempts to hinder its path back to its master.
+	SPELL_FRENZY_RIPLIMB = 100522, //Increases attack speed by 30% and physical damage dealt by 30%.
+	SPELL_LIMB_RIP = 99832, //The hellhound tears viciously at an enemy's exposed flesh, dealing 130% of normal damage and causing a bleeding wound that deals 0 damage every 5 sec for until cancelled. (Triggers Jagged Tear)
+	SPELL_FRENZIED_DEVOLUTION = 100064, //Increases the caster's attack and movement speeds by 200% and all damage it deals by 400%. (Applys some Auras)
+	SPELL_FEEDING_FRENZY_H = 100655, //Successful melee attacks drive the hound into a feeding frenzy, granting a stacking 5% bonus to physical damage dealt for 20 sec.
 
- //Spear Abilities
- SPELL_MAGMA_FLARE = 100495, // Inflicts Fire damage to enemies within 50 yards.
- SPELL_MAGMA_RUPTURE = 100003, // Calls forth magma eruptions to damage nearby foes. (Dummy Effect)
+	//Rageface
+	SPELL_FACE_RAGE = 99947, //Pins an enemy to the ground, stunning them and tearing at their flesh to deal 38 to 42 damage every 0.5 sec for 30 sec.
+	BUFF_FACE_RAGE = 100129, // I'll hope this is correct
+
+	//Spear Abilities
+	SPELL_MAGMA_FLARE = 100495, // Inflicts Fire damage to enemies within 50 yards.
+	SPELL_MAGMA_RUPTURE = 100003, // Calls forth magma eruptions to damage nearby foes. (Dummy Effect)
 
 
 
@@ -119,18 +122,18 @@ enum Spells
 
 enum Events
 {
-   //Shannox
-   EVENT_IMMOLTATION_TRAP,
-   EVENT_BERSERK,
-   EVENT_ARCING_SLASH,
-   EVENT_HURL_SPEAR_OR_MAGMA_RUPTUTRE,
+	//Shannox
+	EVENT_IMMOLTATION_TRAP,
+	EVENT_BERSERK,
+	EVENT_ARCING_SLASH,
+	EVENT_HURL_SPEAR_OR_MAGMA_RUPTUTRE,
 
-   //Riplimb
-   EVENT_LIMB_RIP,
+	//Riplimb
+	EVENT_LIMB_RIP,
 
-   //Rageface
-   EVENT_FACE_RAGE,
-   
+	//Rageface
+	EVENT_FACE_RAGE,
+
 };
 
 // ######### Shannox #########
@@ -140,46 +143,53 @@ class boss_shannox : public CreatureScript
 public:
 	boss_shannox() : CreatureScript("boss_shannox"){}
 
-        CreatureAI* GetAI(Creature* creature) const
-        {
-            return new boss_shannoxAI(creature);
-        }
+	CreatureAI* GetAI(Creature* creature) const
+	{
+		return new boss_shannoxAI(creature);
+	}
 
 	struct boss_shannoxAI : public BossAI
 	{
 		boss_shannoxAI(Creature* c) : BossAI(c, DATA_SHANNOX)
 		{
 			instance = me->GetInstanceScript();
+			// TODO Add not Tauntable Flag
 		}
 
 		InstanceScript* instance;
 		bool enrage;
+		Creature* tempCreature;
 
 		void Reset()
 		{
-			instance->SetBossState(BOSS_SHANNOX, NOT_STARTED);
+			//instance->SetBossState(BOSS_SHANNOX, NOT_STARTED);
 			enrage = false;
 			events.Reset();
-			
-			Creature* tempCreature;
-
+						
 			tempCreature = GetRiplimb();
 			if(tempCreature != 0)
+			{
+				if (tempCreature->isDead())
 				tempCreature -> Respawn();
-						
+			}
+
 			tempCreature = GetRageface();
 			if(tempCreature != 0)
+			{
+				if (tempCreature->isDead())
 				tempCreature -> Respawn();
+			}
+
 
 			//me->SetReactState(REACT_PASSIVE); //TODO Only for testing
 		}
 
 		/*void JustSummoned(Creature* summon)
-        {
-            summons.Summon(summon);
-            summon->setActive(true);
-            summon->AI()->DoZoneInCombat();
-        }*/
+		{
+		summons.Summon(summon);
+		summon->setActive(true);
+		summon->AI()->DoZoneInCombat();
+		}*/
 
 		void KilledUnit(Unit * /*victim*/)
 		{
@@ -187,23 +197,25 @@ public:
 
 		void JustDied(Unit * /*victim*/)
 		{
+			
 			instance->SetBossState(BOSS_SHANNOX, DONE);
+			DoScriptText(SAY_ON_DEAD, me, 0);
 			_JustDied();
 		}
 
-        void EnterCombat(Unit* who)
-        {
+		void EnterCombat(Unit* who)
+		{
 			instance->SetBossState(BOSS_SHANNOX,IN_PROGRESS);
-						
+
 			DoZoneInCombat();
 
 			events.ScheduleEvent(EVENT_IMMOLTATION_TRAP, 20000); //TODO Find out the correct Time
 			events.ScheduleEvent(EVENT_ARCING_SLASH, 10000);  //TODO Find out the correct Time
 			events.ScheduleEvent(EVENT_HURL_SPEAR_OR_MAGMA_RUPTUTRE, 20000); //TODO Find out the correct Time
-			
+
 			events.ScheduleEvent(EVENT_BERSERK, 10 * MINUTE * IN_MILLISECONDS);
 
-			DoScriptText(SAY_AGGRO, me, who);
+			DoScriptText(SAY_AGGRO, me);
 
 			_EnterCombat();
 		}
@@ -216,69 +228,70 @@ public:
 			Unit* tempUnit;
 
 			while (uint32 eventId = events.ExecuteEvent())
-                {
-					switch (eventId)
-                    {
-                        case EVENT_IMMOLTATION_TRAP:
-							tempUnit = SelectTarget(SELECT_TARGET_RANDOM, 1, 500, true);
-							//me->CastSpell(SPELL_IMMOLATION_TRAP, tempUnit->GetPositionX(),tempUnit->GetPositionY(),tempUnit->GetPositionZ());
-							events.RepeatEvent(15000); //TODO Find out the correct Time
-                            break;
-                        case EVENT_ARCING_SLASH:
-							DoCast(SPELL_ARCTIC_SLASH);
-							events.ScheduleEvent(EVENT_ARCING_SLASH, 10000);  //TODO Find out the correct Time
-                            break;
-						case EVENT_HURL_SPEAR_OR_MAGMA_RUPTUTRE:
-							/*if(GetRiplimb()->isDead())
-							{
-								// Magma Rupture when Ripclimb is Death
-								
-							}else
-							{
-								
-								// Hurl Spear when Ripclimb is Alive
-								tempCreature = me->SummonCreature(NPC_SHANNOX_SPEAR,me->GetPositionX(),me->GetPositionY(),me->GetPositionZ()+1);
-								tempCreature->GetMotionMaster()->MoveJump(GetRiplimb()->GetPositionX()+urand(-1,1),
-									GetRiplimb()->GetPositionY()+urand(-1,1),GetRiplimb()->GetPositionZ()+urand(-1,1),1 ,1 );
-							}*/
+			{
+				switch (eventId)
+				{
+				case EVENT_IMMOLTATION_TRAP:
+					tempUnit = SelectTarget(SELECT_TARGET_RANDOM, 1, 500, true);
+					//me->CastSpell(SPELL_IMMOLATION_TRAP, tempUnit->GetPositionX(),tempUnit->GetPositionY(),tempUnit->GetPositionZ());
+					events.RepeatEvent(15000); //TODO Find out the correct Time
+					break;
+				case EVENT_ARCING_SLASH:
+					DoCast(SPELL_ARCTIC_SLASH);
+					events.ScheduleEvent(EVENT_ARCING_SLASH, 10000);  //TODO Find out the correct Time
+					break;
+				case EVENT_HURL_SPEAR_OR_MAGMA_RUPTUTRE:
+					/*if(GetRiplimb()->isDead())
+					{
+					// Magma Rupture when Ripclimb is Death
 
-							DoCast(SPELL_HURL_SPEAR_SUMMON);
-							DoZoneInCombat();
-							DoCast(SPELL_HURL_SPEAR_DUMMY_SCRIPT);
-							DoCast(SPELL_HURL_SPEAR);
+					}else
+					{
+
+					// Hurl Spear when Ripclimb is Alive
+					tempCreature = me->SummonCreature(NPC_SHANNOX_SPEAR,me->GetPositionX(),me->GetPositionY(),me->GetPositionZ()+1);
+					tempCreature->GetMotionMaster()->MoveJump(GetRiplimb()->GetPositionX()+urand(-1,1),
+					GetRiplimb()->GetPositionY()+urand(-1,1),GetRiplimb()->GetPositionZ()+urand(-1,1),1 ,1 );
+					}*/
+
+					DoCast(SPELL_HURL_SPEAR_SUMMON);
+					DoZoneInCombat();
+					DoCast(SPELL_HURL_SPEAR_DUMMY_SCRIPT);
+					DoCast(SPELL_HURL_SPEAR);
 
 
-							events.RepeatEvent(15000); //TODO Find out the correct Time
-                            break;
-						case EVENT_BERSERK:
-							DoCast(me, SPELL_BERSERK);
-                            break;
-					}
+					events.RepeatEvent(15000); //TODO Find out the correct Time
+					break;
+				case EVENT_BERSERK:
+					DoCast(me, SPELL_BERSERK);
+					break;
 				}
+			}
 
 			if (!UpdateVictim())
 				return;
-			
-			/*if((GetRiplimb()->isDead() || GetRageface()->isDead()) && enrage != true)
+
+			if ((GetRiplimb()->isDead() || GetRageface()->isDead()) && !enrage)
 			{
-				DoCast(me, SPELL_BERSERK);
-				me->MonsterTextEmote(SAY_ENRAGE, 0, true);
-				enrage = true;
-			}*/
-		
+			DoCast(me, SPELL_FRENZY_SHANNOX);
+			DoScriptText(SAY_ON_DOGS_FALL, me, 0);
+			me->MonsterTextEmote(SAY_SOFT_ENRAGE, 0, true);
+			enrage = true;
+
 			DoMeleeAttackIfReady();
+			}
 		}
 
 		
 		Creature* GetRiplimb()
-        {
-           return me->GetCreature(*me,instance->GetData64(NPC_RIPLIMB));
-        }
+		{
+			return me->GetCreature(*me,instance->GetData64(NPC_RIPLIMB));
+		}
 
 		Creature* GetRageface()
-        {
-           return me->GetCreature(*me,instance->GetData64(NPC_RAGEFACE));
-        }
+		{
+			return me->GetCreature(*me,instance->GetData64(NPC_RAGEFACE));
+		}
 	};
 };
 
@@ -289,10 +302,10 @@ class npc_rageface : public CreatureScript
 public:
 	npc_rageface() : CreatureScript("npc_rageface"){}
 
-	    CreatureAI* GetAI(Creature* creature) const
-        {
-            return new npc_ragefaceAI(creature);
-        }
+	CreatureAI* GetAI(Creature* creature) const
+	{
+		return new npc_ragefaceAI(creature);
+	}
 
 	struct npc_ragefaceAI : public ScriptedAI
 	{
@@ -325,7 +338,6 @@ public:
 
 		void JustDied(Unit * /*victim*/)
 		{
-			
 		}
 
 		void EnterCombat(Unit * /*who*/)
@@ -340,16 +352,16 @@ public:
 		}
 
 		void DamageTaken(Unit* attacker, uint32 damage)
-			{
-				if (damage >= damageNeededToAbortRageFace/* && me->HasAura(BUFF_FACE_RAGE)*/)
-				{	
-					me->MonsterSay("Triggered",0,0);
-					me->RemoveAurasDueToSpell(SPELL_FACE_RAGE);
-					me->getVictim()->ClearUnitState(UNIT_STAT_STUNNED);
-					me->SetTarget(me->Attack(SelectTarget(SELECT_TARGET_RANDOM, 1, 500, true),true));
-					events.ScheduleEvent(EVENT_FACE_RAGE, 15000); //TODO Find out the correct Time
-				}
+		{
+			if (damage >= damageNeededToAbortRageFace/* && me->HasAura(BUFF_FACE_RAGE)*/)
+			{	
+				me->MonsterSay("Triggered",0,0);
+				me->RemoveAurasDueToSpell(SPELL_FACE_RAGE);
+				me->getVictim()->ClearUnitState(UNIT_STAT_STUNNED);
+				me->SetTarget(me->Attack(SelectTarget(SELECT_TARGET_RANDOM, 1, 500, true),true));
+				events.ScheduleEvent(EVENT_FACE_RAGE, 15000); //TODO Find out the correct Time
 			}
+		}
 
 		void UpdateAI(const uint32 diff)
 		{
@@ -358,44 +370,49 @@ public:
 			events.Update(diff);
 
 			while (uint32 eventId = events.ExecuteEvent())
-                {
-                    switch (eventId)
-                    {
-                        case EVENT_FACE_RAGE:
-							DoCastVictim(SPELL_FACE_RAGE);
-							//me->getVictim()->SetFlag(UNIT_FIELD_FLAGS,  UNIT_STAT_STUNNED);
-							me->getVictim()->AddUnitState(UNIT_STAT_STUNNED);
-							break;
-                    }
+			{
+				switch (eventId)
+				{
+				case EVENT_FACE_RAGE:
+					DoCastVictim(SPELL_FACE_RAGE);
+					//me->getVictim()->SetFlag(UNIT_FIELD_FLAGS,  UNIT_STAT_STUNNED);
+					me->getVictim()->AddUnitState(UNIT_STAT_STUNNED);
+					break;
 				}
+			}
 
 			/*if(GetShannox()->GetHealthPct() <= 30 && frenzy == false)
 			{
-				frenzy = true;
-				DoCast(me, SPELL_FRENZIED_DEVOLUTION);
+			frenzy = true;
+			DoCast(me, SPELL_FRENZIED_DEVOLUTION);
 			}*/
-			
+
 
 			if (!UpdateVictim())
 				return;
-		
+
 			DoMeleeAttackIfReady();
 		}
-		
+
 		void DamageDealt(Unit* /*victim*/, uint32& /*damage*/, DamageEffectType /*damageType*/)
 		{
 			//TODO Set Heroic Condition
 			if (!stackerStopper)
 			{
-			DoCast(me, SPELL_FEEDING_FRENZY_H);
-			stackerStopper = true;
+				DoCast(me, SPELL_FEEDING_FRENZY_H);
+				stackerStopper = true;
 			}
 		}
 
-		/*Creature * GetShannox()
-        {
-           return me->GetCreature(*me,instance->GetData64(BOSS_SHANNOX));
-        }*/
+
+		Creature* GetShannox()
+		{
+			return me->GetCreature(*me,instance->GetData64(NPC_SHANNOX));
+		}
+		UnitAI* GetShannoxAI()
+		{
+			return me->GetCreature(*me,instance->GetData64(NPC_SHANNOX))->GetAI();
+		}
 	};
 };
 
@@ -407,9 +424,9 @@ public:
 	npc_riplimb() : CreatureScript("npc_riplimb"){}
 
 	CreatureAI* GetAI(Creature* creature) const
-    {
-            return new npc_riplimbAI(creature);
-    }
+	{
+		return new npc_riplimbAI(creature);
+	}
 
 	struct npc_riplimbAI : public ScriptedAI
 	{
@@ -424,7 +441,7 @@ public:
 
 		void Reset()
 		{
-			
+
 			me->SetReactState(REACT_PASSIVE); //TODO Only for testing
 		}
 
@@ -450,26 +467,26 @@ public:
 			events.Update(diff);
 
 			while (uint32 eventId = events.ExecuteEvent())
-                {
-                    switch (eventId)
-                    {
-                        case EVENT_LIMB_RIP:
-							DoScriptText(SAY_AGGRO, me);
-							DoCastVictim(SPELL_LIMB_RIP);	
+			{
+				switch (eventId)
+				{
+				case EVENT_LIMB_RIP:
+					DoScriptText(SAY_AGGRO, me);
+					DoCastVictim(SPELL_LIMB_RIP);	
 
-							events.RepeatEvent(urand(10000,20000)); //TODO Find out the correct Time
-                            break;
-                    }
+					events.RepeatEvent(urand(10000,20000)); //TODO Find out the correct Time
+					break;
 				}
+			}
 			/*if(GetShannox()->GetHealthPct() <= 30 && frenzy == false)
 			{
-				frenzy = true;
-				DoCast(me, SPELL_FRENZIED_DEVOLUTION);
+			frenzy = true;
+			DoCast(me, SPELL_FRENZIED_DEVOLUTION);
 			}*/
 
 			if (!UpdateVictim())
 				return;
-		
+
 			DoMeleeAttackIfReady();
 		}
 
@@ -480,10 +497,10 @@ public:
 
 		}
 
-		/*Creature * GetShannox()
-        {
-           return me->GetCreature(*me,instance->GetData64(BOSS_SHANNOX));
-        }*/
+		Creature* GetShannox()
+		{
+			return me->GetCreature(*me,instance->GetData64(NPC_SHANNOX));
+		}
 
 
 	};
@@ -496,10 +513,10 @@ class npc_shannox_spear : public CreatureScript
 public:
 	npc_shannox_spear() : CreatureScript("npc_shannox_spear"){}
 
-	    CreatureAI* GetAI(Creature* creature) const
-        {
-            return new npc_shannox_spearAI(creature);
-        }
+	CreatureAI* GetAI(Creature* creature) const
+	{
+		return new npc_shannox_spearAI(creature);
+	}
 
 	struct npc_shannox_spearAI : public ScriptedAI
 	{
@@ -541,9 +558,9 @@ public:
 		}
 
 		Creature* GetShannox()
-        {
-           return me->GetCreature(*me,instance->GetData64(BOSS_SHANNOX));
-        }
+		{
+			return me->GetCreature(*me,instance->GetData64(NPC_SHANNOX));
+		}
 	};
 };
 
