6243cfbc96b257afe970801037d85b1be4988ea5
Naios <naios-dev@live.de>
2011-12-18 00:11:13 +0100

    Scripts/FL Majordomo Display IDs for Forms added
    Signed-off-by: Naios <naios-dev@live.de>

--- src/Firelands/boss_majordomus.cpp
+++ src/Firelands/boss_majordomus.cpp
@@ -19,12 +19,15 @@
 
 /**********
 * Script Coded by Naios
-* Script Complete 15% (or less)
+* Script Complete 20% (or less)
 **********/
+
+// Scorpion Form: 98% Working
 
 /* TODO 
 
 - Add Sound and Yells
+- Check Display IDs of the Cat and Scorpion Form
 
 */
 
@@ -42,19 +45,21 @@ enum Spells
 	SPELL_CAT_FORM								 = 98374,
 	SPELL_SCORPION_FORM							 = 98379,
 
-	SPELL_ADRENALINE							 = 97238, // Is not needed here i think
+	SPELL_ADRENALINE							 = 97238,
 	SPELL_BERSERK								 = 26662,
 	SPELL_BURNING_ORBS							 = 98451,
 	SPELL_FIERY_CYCLONE							 = 98443,
 	SPELL_FLAME_SCYTE							 = 98474, // 10N
 	SPELL_FURY									 = 97235,
 	SPELL_LEAPING_FLAMES						 = 98476,
-	SPELL_SEARING_SEEDS							 = 98450
+	SPELL_SEARING_SEEDS							 = 98450,
 };
 
 enum Events
 {
-	EVENT_LEAPING_FLAMES = 1
+	EVENT_LEAPING_FLAMES = 1,
+	EVENT_BERSERK		 = 2,
+	EVENT_SWITCH_TO_NEXT_FORM = 3,
 };
 
 enum MajordomoPhase
@@ -62,6 +67,13 @@ enum MajordomoPhase
 	PHASE_DRUID,
 	PHASE_CAT,
 	PHASE_SCORPION
+};
+
+enum Misc
+{
+	DISPLAYID_DRUID								  = 37953, 
+	DISPLAYID_CAT								  = 38747, // The two animals Display ids are needed
+	DISPLAYID_SCORPION							  = 36617,  // because they are not handled through the spells
 };
 
 /**** Majordomo Staghelm ****/
@@ -85,6 +97,8 @@ public:
 
 		InstanceScript* instance;
 		MajordomoPhase phase;
+		MajordomoPhase nextPhase;
+		uint8 DruidPhaseCounterForSearingSeeds;
 
 		void Reset()
 		{
@@ -96,7 +110,6 @@ public:
 			summons.DespawnAll();
 
 			TransformToDruid();
-
 		}
 
 		void KilledUnit(Unit * /*victim*/)
@@ -114,7 +127,6 @@ public:
 
 		void JustDied(Unit * /*victim*/)
 		{
-
 			instance->SetBossState(DATA_MAJORDOMUS, DONE);
 			DoScriptText(SAY_ON_DEAD, me);
 			summons.DespawnAll();
@@ -123,17 +135,21 @@ public:
 
 		void EnterCombat(Unit* who)
 		{
+			DruidPhaseCounterForSearingSeeds = 3;
+
+			events.ScheduleEvent(EVENT_BERSERK, 10 * MINUTE * IN_MILLISECONDS);
+
 			TransformToScorpion();
 			_EnterCombat();
 		}
 
 		void UpdateAI(const uint32 diff)
 		{
-		
+
 			if(phase == PHASE_SCORPION && me->GetPower(POWER_ENERGY) == 100)
 				DoCast(SPELL_FLAME_SCYTE);
 
-			
+
 			if (me->HasUnitState(UNIT_STAT_CASTING))
 				return;
 
@@ -141,39 +157,40 @@ public:
 
 			while (uint32 eventId = events.ExecuteEvent())
 			{
-				switch (phase)
+				switch (eventId)
 				{
 
-				// The Scorpion Phase has no Events
-
-				case PHASE_DRUID:
+					if (phase == PHASE_DRUID)
 					{
-						switch (eventId)
-						{
-
-
-
-
-						}
-					}
-					break;
-
-				case PHASE_CAT:
-					{
-						switch (eventId)
-						{
-						case EVENT_LEAPING_FLAMES:
-
-							// Spell cause Client to Crash
-							//if (Unit* tempTarget = SelectTarget(SELECT_TARGET_RANDOM, 0, 500, true))
-							//	DoCastVictim(SPELL_LEAPING_FLAMES);
-
-							events.ScheduleEvent(EVENT_LEAPING_FLAMES, 20000);
-							break;
-						}
 
 					}
+
+					if (phase == PHASE_CAT)
+					{
+
+				case EVENT_LEAPING_FLAMES:
+
+					// Spell cause Client to Crash
+					//if (Unit* tempTarget = SelectTarget(SELECT_TARGET_RANDOM, 0, 500, true))
+					//	DoCastVictim(SPELL_LEAPING_FLAMES);
+
+					events.ScheduleEvent(EVENT_LEAPING_FLAMES, 20000);
 					break;
+
+					}
+
+				case EVENT_BERSERK:
+					DoCast(me, SPELL_BERSERK,true);
+					break;
+
+				case EVENT_SWITCH_TO_NEXT_FORM:
+
+
+
+
+
+					break;
+
 				}
 			}
 
@@ -185,10 +202,14 @@ public:
 
 
 	private:
-		inline void TransformToCat()
+		void TransformToCat()
 		{
+			me->RemoveAura(SPELL_ADRENALINE);
 			me->RemoveAura(SPELL_SCORPION_FORM);
+			
 			DoCast(me,SPELL_CAT_FORM,false);
+			me->SetDisplayId(DISPLAYID_CAT);
+			
 			me->setPowerType(POWER_ENERGY);
 			me->SetMaxPower(POWER_ENERGY,100);
 			me->SetPower(POWER_ENERGY,0);
@@ -202,10 +223,15 @@ public:
 			phase = PHASE_CAT;
 		}
 
-		inline void TransformToScorpion()
+		void TransformToScorpion()
 		{
+			me->SetFloatValue(OBJECT_FIELD_SCALE_X, 15);
+			me->RemoveAura(SPELL_ADRENALINE);
 			me->RemoveAura(SPELL_CAT_FORM);
+			
 			DoCast(me,SPELL_SCORPION_FORM,false);
+			me->SetDisplayId(DISPLAYID_SCORPION);
+			
 			me->setPowerType(POWER_ENERGY);
 			me->SetMaxPower(POWER_ENERGY,100);
 			me->SetPower(POWER_ENERGY,0);
@@ -217,15 +243,37 @@ public:
 			phase = PHASE_SCORPION;
 		}
 
-		inline void TransformToDruid()
+		void TransformToDruid()
 		{
+			me->RemoveAura(SPELL_ADRENALINE);
 			me->RemoveAura(SPELL_CAT_FORM);
 			me->RemoveAura(SPELL_SCORPION_FORM);
+
+			me->SetFloatValue(OBJECT_FIELD_SCALE_X, 1);
+
+			me->SetDisplayId(DISPLAYID_DRUID);
 
 			me->setPowerType(POWER_MANA);
 			//me->SetMaxPower(POWER_MANA,155555);
 
 			phase = PHASE_DRUID;
+
+
+			// Cast Searing Seeds after Majordoma has switched the 3. time in the Druid Form
+			if(me->isInCombat())
+			{
+				DruidPhaseCounterForSearingSeeds++;
+
+				if(DruidPhaseCounterForSearingSeeds == 4)
+				{
+					DruidPhaseCounterForSearingSeeds = 1;
+
+					DoCast(SPELL_SEARING_SEEDS);
+				}
+
+				events.ScheduleEvent(EVENT_SWITCH_TO_NEXT_FORM, 15000);
+
+			}
 		}
 
 	};
