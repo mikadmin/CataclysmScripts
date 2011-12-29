7189f5bc613de4b3a96fa49c027cb73d37cfe13c
Naios <naios-dev@live.de>
2011-12-29 13:22:50 +0100

    Misc: Updated wrong header
    Signed-off-by: Naios <naios-dev@live.de>

--- src/GrimBatol/boss_erudax.cpp
+++ src/GrimBatol/boss_erudax.cpp
@@ -1,8 +1,4 @@
 /*
-* Copyright (C) 2005 - 2011 MaNGOS <http://www.getmangos.org/>
-*
-* Copyright (C) 2008 - 2011 TrinityCore <http://www.trinitycore.org/>
-*
 * Copyright (C) 2011 ArkCORE <http://www.arkania.net/>
 *
 * This program is free software; you can redistribute it and/or modify it
