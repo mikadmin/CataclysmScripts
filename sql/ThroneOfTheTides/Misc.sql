03ae8edbd6d1c7f2d98a0b86fe04ad96eb67c890
Naios <naios-dev@live.de>
2011-12-30 16:35:05 +0100

    Scripts/TOTT Basement Scripts added
    Signed-off-by: Naios <naios-dev@live.de>

---
+++ sql/ThroneOfTheTides/Misc.sql
@@ -0,0 +1,28 @@
+-- Throne of the Tides Updates
+-- by Naios
+
+-- Gursha removes SmartAI and assign scriptnames
+
+-- Who added SmartAI? ^^
+UPDATE `creature_template` SET `AIName`='', `ScriptName`='boss_erunak_stonespeaker' WHERE `entry`=40825 LIMIT 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 40825;
+
+UPDATE `creature_template` SET `ScriptName`='boss_mindbender_ghursha' WHERE `entry`=40788 LIMIT 1;
+
+-- Neptulon
+UPDATE `creature_template` SET `ScriptName`='boss_neptulon' WHERE `entry`=40792 LIMIT 1;
+
+-- Faction fix
+UPDATE `creature_template` SET `faction_A`=35, `faction_H`=35 WHERE `entry`=40792 LIMIT 1;
+
+
+-- Commander Ulthrok
+UPDATE `creature_template` SET `AIName`='', `ScriptName`='boss_commander_ulthok' WHERE `entry`=40765 LIMIT 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 40765;
+
+-- Lady Nazjar
+-- She is spawned twice...
+DELETE FROM `creature` WHERE `guid` = 1086658;
+
+UPDATE `creature_template` SET `AIName`='', `ScriptName`='boss_lady_nazjar' WHERE `entry`=40586 LIMIT 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 40586;
