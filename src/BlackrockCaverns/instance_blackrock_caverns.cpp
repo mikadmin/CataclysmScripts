f3a73ebd42bf352bffb6f41f3dcc71d058453801
Naios <naios-dev@live.de>
2012-01-06 20:30:59 +0100

    Misc: Header changed
    Signed-off-by: Naios <naios-dev@live.de>

--- src/BlackrockCaverns/instance_blackrock_caverns.cpp
+++ src/BlackrockCaverns/instance_blackrock_caverns.cpp
@@ -1,6 +1,8 @@
 /*
 * Copyright (C) 2010-2011 Project SkyFire <http://www.projectskyfire.org/>
 *
+* Copyright (C) 2011 ArkCORE <http://www.arkania.net/>
+*
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
 * Free Software Foundation; either version 2 of the License, or (at your
