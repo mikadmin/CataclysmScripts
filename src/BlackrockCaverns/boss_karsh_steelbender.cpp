4b9bf300d69eaef674004eb899bba52e6a7eaca4
Naios <naios-dev@live.de>
2012-01-06 02:20:00 +0100

    Scripts/BC Finished Karsh Steelbender
    Signed-off-by: Naios <naios-dev@live.de>

--- src/BlackrockCaverns/boss_karsh_steelbender.cpp
+++ src/BlackrockCaverns/boss_karsh_steelbender.cpp
@@ -17,8 +17,12 @@
 
 /**********
 * Script Coded by Naios
-* Script Complete 50% (or less)
+* Script Complete 85% (or less)
 **********/
+
+/* Notes:
+- I don't now weather the lava pool handling (spell) is blizzlike
+*/
 
 #include "ScriptPCH.h"
 #include "blackrock_caverns.h"
@@ -32,12 +36,22 @@ enum Spells
 	SPELL_BURNING_METAL = 76002,
 	SPELL_CLEAVE = 845,
 	SPELL_LAVA_SPOUT = 76007,
+
+	SPELL_LAVA_POOL = 93628,
 };
 
 enum Events
 {
 	EVENT_CLEAVE = 1,
 	EVENT_CHECK_ARMOR_STATE = 2,
+	EVENT_LAVA_POOL = 3,
+};
+
+Position const summonPositions[3] =
+{
+	{268.717f, 789.984f, 95.3499f, 4.86041f},
+	{226.707f, 754.725f, 95.3501f, 2.75163f},
+	{216.941f, 808.943f, 95.35f, 0.638911f},
 };
 
 class boss_karsh_steelbender : public CreatureScript
@@ -55,15 +69,27 @@ public:
         boss_karsh_steelbenderAI(Creature* creature) : ScriptedAI(creature)
         {
             instance = creature->GetInstanceScript();
+
+			std::list<Creature*> creatures;
+			GetCreatureListWithEntryInGrid(creatures, me, NPC_LAVA_SPOUT_TRIGGER, 1000.0f);
+
+			if (creatures.empty())
+				return;
+
+			for (std::list<Creature*>::iterator iter = creatures.begin(); iter != creatures.end(); ++iter)
+				(*iter)->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_DISABLE_MOVE | UNIT_FLAG_NOT_SELECTABLE);
         }
 
         InstanceScript* instance;
 		EventMap events;
+		uint8 SpawnCount;
 
         void Reset()
 		{
 			events.Reset();
 			me->RemoveAllAuras();
+
+			DespawnCreatures(NPC_BOUND_FLAMES);
 		}
 
         void EnterCombat(Unit* /*who*/)
@@ -71,6 +97,8 @@ public:
 			DoCast(me, SPELL_QUECKSILVER_ARMOR);
 			events.ScheduleEvent(EVENT_CLEAVE, urand(15000,18000));
 			events.ScheduleEvent(EVENT_CHECK_ARMOR_STATE, 1000);
+
+			me->MonsterYell("Bodies to test my armaments upon!", LANG_UNIVERSAL, NULL);
 		}
 
         void UpdateAI(const uint32 diff)
@@ -83,8 +111,21 @@ public:
 
 				LavaSpoutErrupt();
 
+				me->MonsterYell("Feel the burn!", LANG_UNIVERSAL, NULL);
+
 				DoCast(me, SPELL_QUECKSILVER_ARMOR);
 
+				if(!me->GetMap()->IsHeroic())
+					return;
+
+				// Heroic: Summon Adds
+
+				uint8 r = urand(0,2);
+
+				for(uint8 i = 0; i<=2; i++)
+					me->SummonCreature(NPC_BOUND_FLAMES,summonPositions[r], TEMPSUMMON_CORPSE_DESPAWN);
+
+				SpawnCount = 3;
 
 				return;
 			}
@@ -103,10 +144,15 @@ public:
 				case EVENT_CHECK_ARMOR_STATE:
 					// Checks weather the Boss is in heat range
 
-					if(me->GetDistance2d(237.166f, 785.067f /*Stream of Molten*/) < 2.5f)
+					if(me->GetDistance(237.166f, 785.067f, 95.67f /*Stream of Molten*/) < 4.5f)
 					{
 						me->RemoveAura(SPELL_QUECKSILVER_ARMOR);
+						
+						// We cant do that with CastSpell because with the Spell the Normal Armor is applied too
 						me->SetAuraStack(SPELL_SUPERHEATED_QUECKSILVER_ARMOR,me,me->GetAuraCount(SPELL_SUPERHEATED_QUECKSILVER_ARMOR)+1);
+						me->GetAura(SPELL_SUPERHEATED_QUECKSILVER_ARMOR)->RefreshDuration();
+
+						DoCastAOE(SPELL_HEAT_WAVE);
 					}
 
 					events.ScheduleEvent(EVENT_CHECK_ARMOR_STATE, 1000);
@@ -123,7 +169,35 @@ public:
 				DoCastVictim(SPELL_BURNING_METAL);
 		}
 
+		void SummonedCreatureDespawn(Creature* summon)
+		{
+			if(summon->GetEntry() == NPC_BOUND_FLAMES)
+			{
+			SpawnCount--;
+
+			if(SpawnCount == 0)
+				DoCastAOE(SPELL_LAVA_POOL,true);			
+			}
+		}
+
+		void JustDied(Unit* /*killer*/)
+		{
+			DespawnCreatures(NPC_BOUND_FLAMES);
+			me->MonsterYell("Merely an impurity in the compound...", LANG_UNIVERSAL, NULL);
+		}
+
 	private:
+		void DespawnCreatures(uint32 entry)
+		{
+			std::list<Creature*> creatures;
+			GetCreatureListWithEntryInGrid(creatures, me, entry, 1000.0f);
+
+			if (creatures.empty())
+				return;
+
+			for (std::list<Creature*>::iterator iter = creatures.begin(); iter != creatures.end(); ++iter)
+				(*iter)->DespawnOrUnsummon();
+		}
 
 		void LavaSpoutErrupt()
 		{
